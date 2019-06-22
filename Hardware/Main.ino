249dbaa54cd889059a66d633877f05de2a41c36d
Levi Aviv <leviaviv28@gmail.com>
2019-06-21 23:13:18 -0400

    Added pairing logic starter code, and added limit setting in EEPROM

--- Hardware/Main.ino
+++ Hardware/Main.ino
@@ -1,28 +1,55 @@
+#include "SwitchPack.h"
 #include <EEPROM.h>
+#include <SoftwareSerial.h>
 
+#define hit_lim_addr 0
+#define cur_hit_addr 1
 #define btnPin 13
 #define ledPin 12
+#define btStatePin 9
+#define REACTION_TIME 500
 
-int firstPress = 999;
-int secondPress = 999;
-int thirdPress = 999;
+DoubleClick key(btnPin, PULLUP, REACTION_TIME);
+SoftwareSerial EEBlue(10, 11); // RX | TX
+byte btnPresses = 0;
 
-byte btnState = LOW;
+void blinkLed(){
+    digitalWrite(ledPin, HIGH);
+    delay(50);
+    digitalWrite(ledPin, LOW);
+}
+
 
 void setup(){
     Serial.begin(9600);
-    pinMode(btnPin, INPUT);
+    EEBlue.begin(9600);
+    key.begin();
+    key.setMaxClicks(3);
     pinMode(ledPin, OUTPUT);
+    EEPROM.write(hit_lim_addr, 10);
+    EEPROM.write(cur_hit_addr, 9);
 }
 
 void loop(){
-    btnState = (digitalRead(btnPin) == 0)? HIGH : LOW;
-    digitalWrite(ledPin, btnState);
-    int timeout = 0;
-    while(timeout > 500){
-        
+    btnPresses = key.clickCount();
+    if(btnPresses == 1){
+        if(EEPROM.read(cur_hit_addr) < EEPROM.read(hit_lim_addr)){
+            blinkLed();
+            EEPROM.write(cur_hit_addr, EEPROM.read(cur_hit_addr) + 1);
+            Serial.println("Hit allowed");
+        } else {
+            Serial.println("Hit not allowed");
+        }
+    } else if(btnPresses == 3){
+        Serial.println("Pairing Mode");
+        blinkLed();
+        delay(100);
+        blinkLed();
+        delay(100);
+        blinkLed();
+        while(!digitalRead(btStatePin)){
+            delay(50);
+        }
+        Serial.println("Paired");
     }
-    firstPress = 999;
-    secondPress = 999;
-    thirdPress = 999;
 }
