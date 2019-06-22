59e05c54db3cf65b4bebc9355b6171094207a124
Jarrod Servilla <jarrod.servilla@gmail.com>
2019-06-22 16:00:34 -0400

    Completed saving functionality

--- AndroidApp/Questionnaire.kt
+++ AndroidApp/Questionnaire.kt
@@ -1,11 +1,8 @@
 package com.example.vapesafe
 
-import android.content.Context
 import android.content.Context.MODE_PRIVATE
 import android.content.SharedPreferences
-import android.net.Uri
 import android.os.Bundle
-import android.provider.Telephony.Mms.Part.FILENAME
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -15,8 +12,6 @@ import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.Spinner
-import com.google.gson.Gson
-import kotlinx.android.synthetic.main.nav_header_main.view.*
 import kotlinx.android.synthetic.main.questionnaire_fragment.view.*
 
 class Questionnaire : Fragment() {
@@ -39,83 +34,21 @@ class Questionnaire : Fragment() {
         val spinnerq4: Spinner = view.spinner_q4
         createSpinner(spinnerq4, R.array.qa_q4_choices, view)
 
-        var select1 = ""
-        var select2 = ""
-        var select3 = ""
-        var select4 = ""
+        val submit = view.submitButton
 
-        spinnerq1.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-
-            }
-
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                if (parent != null) {
-                    select1 = parent.getItemAtPosition(position).toString()
-                    Log.d("MainActivity", select1)
-                }
-            }
-
-        }
-
-        spinnerq2.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-
-            }
-
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                if (parent != null) {
-                    select2 = parent.getItemAtPosition(position).toString()
-                }
-            }
-
-        }
-
-        spinnerq3.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-
-            }
-
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                if (parent != null) {
-                    select3 = parent.getItemAtPosition(position).toString()
-                }
-            }
-
-        }
-
-        spinnerq4.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-
-            }
-
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                if (parent != null) {
-                    select4 = parent.getItemAtPosition(position).toString()
-                }
-            }
-
-        }
-
-        val clickListener = View.OnClickListener {
-            val sharedPreferences: SharedPreferences = view.context.getSharedPreferences(FILENAME, MODE_PRIVATE)
+        submit.setOnClickListener {
+            val sharedPreferences: SharedPreferences = context?.getSharedPreferences(FILENAME, MODE_PRIVATE)!!
             val editor: SharedPreferences.Editor = sharedPreferences.edit()
 
-            editor.putString("q1", select1)
-            editor.putString("q2", select2)
-            editor.putString("q3", select3)
-            editor.putString("q4", select4)
-            editor.commit()
+            editor.putString("q1", spinnerq1.selectedItem.toString())
+            editor.putString("q2", spinnerq2.selectedItem.toString())
+            editor.putString("q3", spinnerq3.selectedItem.toString())
+            editor.putString("q4", spinnerq4.selectedItem.toString())
+            editor.apply()
 
-            Log.d("MainActivity", select1)
-            Log.d("MainActivity", sharedPreferences.getString("q1", "").toString())
+            Log.d("MainActivity", "DATA SAVED")
 
         }
-
-        val submitButton: Button = Button(view.context)
-        submitButton.setOnClickListener(clickListener)
-
-
 
         return view
     }
