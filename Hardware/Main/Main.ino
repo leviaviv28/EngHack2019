d29cbd4d155113958c7ad9c8fabcd4cc13ab04b0
Levi Aviv <leviaviv28@gmail.com>
2019-06-22 23:09:25 -0400

    Final code push

--- Hardware/Main/Main.ino
+++ Hardware/Main/Main.ino
@@ -10,6 +10,7 @@
 #define hit_start_addr 5
 #define btnPin 13
 #define ledPin 12
+#define ledPin2 10
 #define btStatePin 11
 #define REACTION_TIME 500
 
@@ -17,12 +18,12 @@ DoubleClick key(btnPin, PULLUP, REACTION_TIME);
 byte btnPresses = 0;
 String btData = "";
 
-void blinkLed(int duration, int n){
+void blinkLed(byte pin, int duration, int n){
     for(int i = 0; i < n; i++){
-        digitalWrite(ledPin, HIGH);
+        digitalWrite(pin, HIGH);
         long start = millis();
         while(millis() - start < duration){}
-        digitalWrite(ledPin, LOW);
+        digitalWrite(pin, LOW);
     }
 }
 
@@ -31,6 +32,8 @@ void syncData(){
     while(btSerial.available() > 0){
         btData += btSerial.read();
     }
+    EEPROM.write(hit_lim_addr, btData.toInt());
+    Serial.print("New limit set to: ");
     Serial.println(btData);
     btData = "";
     btData += EEPROM.read(sync_year_addr);
@@ -39,7 +42,7 @@ void syncData(){
     btData += ",";
     btData += EEPROM.read(sync_day_addr);
     btData += "/";
-    for(int i = 0; i < 5; i++){
+    for(int i = 0; i < EEPROM.read(day_offset); i++){
         btData += EEPROM.read(hit_start_addr + i);
         btData += ",";
     }
@@ -47,10 +50,6 @@ void syncData(){
     Serial.println();
     Serial.println(btData);
     btSerial.print(btData);
-    btData = "";
-    while(btSerial.available() > 0){
-        btData += btSerial.read();
-    }
     btSerial.end();
 }
 
@@ -59,41 +58,45 @@ void setup(){
     key.begin();
     key.setMaxClicks(3);
     pinMode(ledPin, OUTPUT);
-    EEPROM.write(hit_lim_addr, 100);
+    pinMode(ledPin2, OUTPUT);
+    EEPROM.write(hit_lim_addr, 25);
     EEPROM.write(sync_year_addr, 19);
     EEPROM.write(sync_month_addr, 6);
     EEPROM.write(sync_day_addr, 22);
-    EEPROM.write(day_offset, 5);
-    for(int i = 0; i < 5; i++){
-        EEPROM.write(hit_start_addr + i, i * 10);
-    }
+    EEPROM.write(day_offset, 8);
+
+    //setting values for demo
+    EEPROM.write(EEPROM.read(hit_start_addr + EEPROM.read(day_offset)), 24);
 }
 
 void loop(){
     //One Day has passed, increment day counter
-    if((millis() / 100) % 86400 == 0){
-        EEPROM.write(day_offset, EEPROM.read(day_offset) + 1);
+    if(millis() % 8640000 == 0){
+        //EEPROM.write(day_offset, EEPROM.read(day_offset) + 1);
+        Serial.println("Day has passed");
     }
+
     btnPresses = key.clickCount();
-    
-    if(btnPresses == 1){
-        Serial.println(btnPresses);
-        if(EEPROM.read(hit_start_addr) + EEPROM.read(day_offset) < EEPROM.read(hit_lim_addr)){
-            blinkLed(1000, 1);
-            EEPROM.write(EEPROM.read(hit_start_addr) + EEPROM.read(day_offset), EEPROM.read(hit_start_addr) + EEPROM.read(day_offset));
-            Serial.println("Hit allowed");
+    if(btnPresses == 1) {
+        if(EEPROM.read(EEPROM.read(day_offset)) <= EEPROM.read(hit_lim_addr)){
+            blinkLed(ledPin, 1000, 1);
+            Serial.print("Vaporizer hits left: ");
+            Serial.println(EEPROM.read(hit_lim_addr) - EEPROM.read(EEPROM.read(day_offset)));
+            EEPROM.write(EEPROM.read(day_offset), EEPROM.read(EEPROM.read(day_offset)) + 1);
         } else {
-            Serial.println("Hit not allowed");
+            blinkLed(ledPin2, 1000, 1);
+            Serial.println("Vaporizer hit not allowed");
         }
-    } else if(btnPresses == 3){
+    } else if(btnPresses == 3) {
         Serial.println("Pairing Mode");
-        blinkLed(3, 500);
+        blinkLed(ledPin, 3, 500);
         btSerial.begin(9600);
         while(!digitalRead(btStatePin)){
-            blinkLed(100, 1);
+            blinkLed(ledPin, 100, 1);
         }
         Serial.println("Paired");
         Serial.println("Sending");
+        delay(1000);
         syncData();
     }
 }
